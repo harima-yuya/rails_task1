ce80b860abdc9d63e98a25bd4768d73719a40a20
harima-yuya <yuya0203.h@gmail.com>
2022-07-13 14:17:07 +0900

    addのviewを作成、ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   get "posts", to: "posts#index"
   get 'posts/add'
+  post "posts/add", to: "posts#create"
   get 'posts/edit'
   get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
